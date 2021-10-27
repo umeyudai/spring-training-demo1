64320207141ab98d2d8833a384688f40dc70d752
umeyudai <ume.yudai@gmail.com>
2021-10-27 10:40:26 +0900

    inital commit

---
+++ src/main/java/com/umeyudai/springbootdemo1/hello/Employee.java
@@ -0,0 +1,9 @@
+package com.umeyudai.springbootdemo1.hello;
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String employeeId;
+    private String employeeName;
+    private int employeeAge;
+}
